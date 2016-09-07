bbbbd2f3ea3317f5e04003a310d4377787dd37de
Yashwant Patel <yyashwantpatel@gmail.com>
2016-09-07 08:51:09 +0530

    route,ui-bootstrap,ng-animation added to repo

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+(function () {
+    var app = angular.module('bootsrapDrapDown', ['ngRoute', 'ui.bootstrap', "ngAnimate", "ngDragDrop"]);
+    app.controller("dragDropBody", ['$scope', function ($scope) {
+
+        $scope.name = "Yahswantr";
+        }]);
+
+})()
