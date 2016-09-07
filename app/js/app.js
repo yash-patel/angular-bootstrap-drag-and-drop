0f385ef7866f767b4db2b3d5736a9ed54e0cd05f
Yashwant Patel <yyashwantpatel@gmail.com>
2016-09-07 09:01:17 +0530

    Basic drag drop added

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,11 @@
     var app = angular.module('bootsrapDrapDown', ['ngRoute', 'ui.bootstrap', "ngAnimate", "ngDragDrop"]);
     app.controller("dragDropBody", ['$scope', function ($scope) {
 
-        $scope.name = "Yahswantr";
+        $scope.name = "Basic Drag and Drop";
+        $scope.list1 = {
+            title: 'AngularJS - Drag Me'
+        };
+        $scope.list2 = {};
         }]);
 
 })()
